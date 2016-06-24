7ff09992e2c78083ef30e9eec90b35d1d91696d0
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-06-24 19:19:46 +0300

    init commit. used boilerplate https://github.com/davezuko/react-redux-starter-kit added page for table task

---
+++ src/components/Klika/Counter.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react'
+import classes from './Counter.scss'
+
+class Counter extends React.Component {
+
+    render () {
+        const props = this.props
+        return (<div>
+           "Hi, Klika
+        </div>)
+    }
+
+}
+
+export default Counter
